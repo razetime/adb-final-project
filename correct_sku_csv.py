f617528b0c43e664658b6c2f985258fc07c9990d
razetime <rraghu.11502@gmail.com>
2023-12-13 15:42:39 +0800

    correct sku import

--- correct_sku_csv.py
+++ correct_sku_csv.py
@@ -26,21 +26,23 @@ with open("e-commerce_data\\電商數據\\2_商品檔\\(商品主檔)sku_2012041
                 user = username,
                 password = pwd,
                 port = port_id) as conn:
-    cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     header = next(csv.reader([infile.readline()]))
     for idx, line in enumerate(infile):
-        row = next(csv.reader([line]))
-        if len(row) != len(header):
-            continue
-        # new_row = [*row[0:3],*findcd(row[3]),row[4],*findcd(row[5])]
-        # final_row = []
-        # for i in new_row:
-        #     if i == '':
-        #         final_row.append(None)
-        #     else:
-        #         final_row.append(i) 
-        # print(final_row[1])
-        try:
-            cur.execute("insert into sku_20120413 values (%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
-        except:
-            print("err", idx)
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            row = next(csv.reader([line]))
+            if len(row) != len(header):
+                continue
+            # new_row = [*row[0:3],*findcd(row[3]),row[4],*findcd(row[5])]
+            # final_row = []
+            # for i in new_row:
+            #     if i == '':
+            #         final_row.append(None)
+            #     else:
+            #         final_row.append(i) 
+            # print(final_row[1])
+            try:
+                cur.execute("insert into sku_20120413 values (%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
+            except Exception as e:
+                conn.rollback()
+                print("err", idx)
+                print(e)
