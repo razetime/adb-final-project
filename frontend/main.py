16453843a07805f22ec1be6e59b621ec26a5f4bc
razetime <rraghu.11502@gmail.com>
2023-12-23 17:18:06 +0800

    basic psql query+html site

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from psql_query import *
 import random
 import requests
 
@@ -10,7 +11,6 @@ def home():
 
 @app.route('/repeated_purchases')
 def repeated_purchases():
-
     cities = ['新北', '新社', '高門', '鄰環', '昌路', '苗栗', '長門', '大里', '高雄', '南投', '際富', '中村', '埔門', '螺鎮', '村新', '時村', '鄉高', '新都', '西路', '鮮超', '傳門', '鎮美', '馬公', '苑門', '大學', '永康', '鄉南', '里新', '仙門', '林超', '成門', '圍魚', '療門', '吉村', '學門', '岐超', '生路', '康超', '鄉後', '華門', '村盛', '池門', '中壢', '山門', '埔榮', '專前', '零售', '會門', '安里', '好超', '寶門', '巢鄉', '福門', '林縣', '東路', '嘉義', '第一', '里環', '青超', '台中', '集鎮', '玉田', '上鄉', '村南', '鄉新', '冠門', '路舊', '宜蘭', '仁門', '第二', '訓門', '上村', '美村', '豊里', '青門', '洲門', '泰門', '中門', '藥門', '金門', '洋門', '鎮環', '綜合', '斗六', '產品', '鄰新', '花蓮', '縣新', '購門', '興里', '昌門', '花門', '運門', '竹北', '欣超', '新竹', '朴子', '廣門', '美門', '降門', '村村', '德門', '果菜', '多摩', '臺南', '鄉中', '冠超', '鎮元', '台南', '樂村', '堂村', '鎮新', '大門', '份門', '文山', '燕村', '豐原', '豐里', '勝門', '大超', '心門', '里西', '太平', '園門', '山村', '臺中', '鳳山', '後村', '南環', '河鎮', '園超', '民超', '樹門', '境門', '台東', '楊梅', '林門', '內埔', '二段', '八德', '嵐門', '寮門', '平鎮', '民生', '豐門', '禮門', '村米', '彰化', '學都', '鎮中', '里美', '督門', '基隆', '新營', '慶門', '尾門', '海門', '屏東', '祥門', '台北', '山路', '桃園', '軒門', '南興', '公有', '街新', '鎮西', '會超', '太保', '興門', '大成', '梅鎮', '雅村', '路新']
     # products = ["VivoBook Pro 15", "ROG Zephyrus S17"]
 
@@ -52,33 +52,53 @@ def update_map_repeated_purchases():
     print("Received data:", data)  # This line prints the received data to the console
     city = data.get('city')
     # supplier_id = data.get('supplier_id')  # New field for Supplier ID
-    product_id = data.get('product-id')
+    product_id = data.get('product_id') # 1199969
     timeframe = data.get('timeframe')
     year = data.get('year')  # New field for Year
     quarter = data.get('quarter')  # New field for Quarter
     month = data.get('month')  # New field for Month
+    months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
 
     # Fetch GeoJSON
     url = 'https://api.maptiler.com/data/09e09097-be00-42f9-ae5e-af6db6815443/features.json?key=uIMFZoCyLmKWTvmPj2JG'
-    response = requests.get(url)
-    geojson_data = response.json()
+    # response = requests.get(url)
+    # geojson_data = response.json()
+    print(int(year),quarter[1],[months.index(month)+1,product_id])
+    geojson_data = None
+    match timeframe:
+        case 'Monthly':
+            geojson_data = query_monthly(int(year),quarter[1],[months.index(month)+1,product_id])
+        case 'Quarterly':
+            geojson_data = query_quarterly(int(year),quarter[1],[product_id])
+        case 'Yearly':
+            # query_yearly(int(year),[product_id])
+            geojson_data = []
+            pass
+        
 
     # Process GeoJSON
     processed_data = []
-    for feature in geojson_data['features']:
-        district = feature['properties']['District']
-        repeated_purchases = feature['properties']['Repeated Purchases']
+    # for feature in geojson_data['features']:
+    for feature in geojson_data:
+        feature=feature[0]
+        city = feature['properties']['city']
+        # district = feature['properties']['district']
+        repeated_purchases = feature['properties']['count']
         coordinates = feature['geometry']['coordinates']
 
         processed_data.append({
-            'district': district,
+            # 'district': district,
+            'city': city,
             'repeated_purchases': repeated_purchases,
             'coordinates': coordinates
         })
 
     # Calculate min and max repeated purchases
-    min_purchases = min(int(feature['properties']['Repeated Purchases']) for feature in geojson_data['features'])
-    max_purchases = max(int(feature['properties']['Repeated Purchases']) for feature in geojson_data['features'])
+    # min_purchases = min(int(feature['properties']['Repeated Purchases']) for feature in geojson_data['features'])
+    # max_purchases = max(int(feature['properties']['Repeated Purchases']) for feature in geojson_data['features'])
+    max_purchases = max(int(feature[0]['properties']['count']) for feature in geojson_data)
+    min_purchases = min(int(feature[0]['properties']['count']) for feature in geojson_data)
 
     # Prepare and return the response data
     response_data = {
@@ -88,6 +108,7 @@ def update_map_repeated_purchases():
         'min_purchases': min_purchases,  # Minimum repeated purchases value
         'max_purchases': max_purchases   # Maximum repeated purchases value
     }
+    print(response_data)
     return jsonify(response_data)
 
 
